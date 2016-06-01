59ba3b38af60d63bb312e611e25154cc4123b718
Nasreddine Houria <Nasreddine.Houria@gmail.com>
2016-06-01 22:40:44 +0200

    Added project files

---
+++ WorldMapViewer/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WorldMapViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CountryMouseEnter(object sender, MouseEventArgs e)
+        {
+            var path = sender as Path;
+            if (path != null)
+            {
+                path.Fill = new SolidColorBrush(Colors.Aqua);
+            }
+        }
+
+        private void Country_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var path = sender as Path;
+            if (path != null)
+            {
+                path.Fill = new SolidColorBrush(Colors.Black);
+            }
+        }
+    }
+}
